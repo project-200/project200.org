6fe88bba135c3be3ae3cc0cb7fae0d9ef06ea685
Saksham Saini <sakshamsaini18@gmail.com>
2024-12-10 07:24:19 -0500

    Add team and contact pages, contact us form

---
+++ src/app/team/page.tsx
@@ -0,0 +1,17 @@
+import CoreValues from "@/components/ui/coreValues";
+import HeroSection from "@/components/ui/heroSection";
+import TeamSection from "@/components/ui/team";
+
+export default function Team() {
+  return (
+    <div>
+      <HeroSection
+        imageUrl="/team_hero.png"
+        title="People Committed to Changing Lives"
+        description="Get to know the dedicated individuals who are driving our mission to uplift those in need."
+      />
+      <TeamSection />
+      <CoreValues />
+    </div>
+  );
+}
